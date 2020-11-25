130b2398e9de70d0e4c244e1f7392bf9cf2e2bd7
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2020-11-25 11:14:59 +0800

    [modified] black format

--- src/eval.py
+++ src/eval.py
@@ -11,31 +11,30 @@ logger = logging.getLogger(__name__)
 
 
 def f1_score(y_true, y_pred, scheme, average="micro"):
-    """Compute the F1 score.
-    """
+    """Compute F1 score."""
     nb_correct, nb_pred, nb_true = _calculate(y_true, y_pred, scheme)
     p = nb_correct / nb_pred if nb_pred > 0 else 0
     r = nb_correct / nb_true if nb_true > 0 else 0
     score = 2 * p * r / (p + r) if p + r > 0 else 0
     return score
 
-def precision_score(y_true, y_pred, scheme, average='micro'):
-    """Compute the precision.
-    """
+
+def precision_score(y_true, y_pred, scheme, average="micro"):
+    """Compute precision score."""
     nb_correct, nb_pred, _ = _calculate(y_true, y_pred, scheme)
     score = nb_correct / nb_pred if nb_pred > 0 else 0
     return score
 
-def recall_score(y_true, y_pred, scheme, average='micro'):
-    """Compute the recall.
-    """
+
+def recall_score(y_true, y_pred, scheme, average="micro"):
+    """Compute recall score."""
     nb_correct, _, nb_true = _calculate(y_true, y_pred, scheme)
     score = nb_correct / nb_true if nb_true > 0 else 0
     return score
 
+
 def accuracy_score(y_true, y_pred, scheme):
-    """Accuracy classification score.
-    """
+    """Compute accuracy score."""
     if any(isinstance(s, list) for s in y_true):
         y_true = [item for sublist in y_true for item in sublist]
         y_pred = [item for sublist in y_pred for item in sublist]
@@ -43,6 +42,7 @@ def accuracy_score(y_true, y_pred, scheme):
     nb_true = len(y_true)
     score = nb_correct / nb_true
     return score
+
 
 def _calculate(y_true, y_pred, scheme):
     true_entities = set(EntityFromList(y_true, scheme, eval=True).entities)
